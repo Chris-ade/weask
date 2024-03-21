96a68749a6e3c9b3f3eeedb3c25dcfa9563d6813
Chris Adebiyi <christopherolatunjiade@gmail.com>
2024-03-21 19:16:50 +0100

    fixing conflicts

--- README.md
+++
@@ -1,2 +0,0 @@
-## WeAsk Frontend
-This is the branch containing the frontend source code
--- src/utils/TitleCase.js
+++
@@ -1,5 +0,0 @@
-export default function toTitleCase({ text }) {
-  return text.replace(/\b\w/g, (char) => {
-    return char.toUpperCase();
-  });
-}
